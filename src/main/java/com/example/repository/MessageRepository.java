98cad03837ebbfd876e735a90bc1c849c2f8776f
Said Said <ssaid20@icloud.com>
2025-02-03 15:45:19 +0000

    added MessageRepository

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,1 +1,11 @@
+package com.example.repository;
 
+import com.example.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    List<Message> findByPostedBy(Integer postedBy);
+}
